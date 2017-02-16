32e3a315978a04bf2c6d5236ec3afba7207a11e1
angular-cli <angular-cli@angular.io>
2017-02-17 00:28:09 +0100

    chore: initial commit from angular-cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { UnittestPage } from './app.po';
+
+describe('unittest App', function() {
+  let page: UnittestPage;
+
+  beforeEach(() => {
+    page = new UnittestPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
