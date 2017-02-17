a50129022a25077726633fd04908288fb4e816be
mahdikallel <mahdikallel>
2017-02-17 10:58:23 +0100

    adding another test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -11,4 +11,9 @@ describe('unittest App', function() {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('app works!');
   });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
 });
