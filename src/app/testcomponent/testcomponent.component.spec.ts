27e53c5280c0e1d81bb973e2a62dddaf87aaa9f3
mahdikallel <mahdikallel>
2017-02-17 11:50:46 +0100

    commentaire unit test

--- src/app/testcomponent/testcomponent.component.spec.ts
+++ src/app/testcomponent/testcomponent.component.spec.ts
@@ -55,6 +55,6 @@ describe('TestcomponentComponent', () => {
   it('it should  decrement myNumber', () => {
     testComp.downNumber();
     expect(testComp.myNumber).toBe(0);
-    /**/
+    /*unit test */
   });
 });
